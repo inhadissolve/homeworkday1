897df93801cc9817220786560d13c8ed8e38f152
dissolve <dissolve1882@naver.com>
2025-02-14 17:48:57 +0900

    chanv1.0 day5

--- day06.py
+++ day06.py
@@ -1,25 +1,27 @@
 class FlyingMixin:
     def fly(self):
-        return f"{self.hidden_name}이(가) 하늘을 훨훨 날아갑니다~"
+        return f"{self.__name}이(가) 하늘을 훨훨 날아갑니다~"
 
 class SwimmingMixin:
     def swim(self):
-        return f"{self.hidden_name}이(가) 수영을 합니다."
+        return f"{self.__name}이(가) 수영을 합니다."
 
 class Pokemon:
     def __init__(self, name):
-        self.hidden_name = name
+        self.__name = name
 
     def attack(self):
         print("공격~")
 
-    def get_name(self):
-        return self.hidden_name
+    @property
+    def name(self):
+        return self.__name
 
-    def set_name(self, new_name):
-        self.hidden_name = new_name
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
 
-    name = property(get_name, set_name)
+    #name = property(get_name, set_name)
 
 
 class Charizard(Pokemon, FlyingMixin):
@@ -35,7 +37,10 @@ c1 = Charizard("리자몽")
 # g1.set_name("잉어킹")
 # print(g1.get_name())
 
-# property
+# property 3rd
 print(g1.name)
+#print(g1.__name)  # direct access X
+#g1._Pokemon__name = "잉어킹"
 g1.name = "잉어킹"
-print(g1.name)
+#g1.__name = "잉어킹"
+print(g1._Pokemon__name)  # 사실 상 private 개념은 없는 걸로
