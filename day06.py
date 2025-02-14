051179da6e49f76ce38bba47883543333cfed463
dissolve <dissolve1882@naver.com>
2025-02-14 17:26:13 +0900

    chan0.8 day5

--- day06.py
+++ day06.py
@@ -1,4 +1,7 @@
-def my_abs(n) -> int:
-    if n < 0:
-        return -n
-    return n
+import tgmath
+
+print(tgmath.my_abs(-99))
+
+
+if __name__ == "__main__":
+    print("tgmath.py 파일을 실행하셨습니다")
---
+++ tgmath.py
@@ -0,0 +1,7 @@
+def my_abs(n) -> int:
+    if n < 0:
+        return -n
+    return n
+
+if __name__ != "__main__":
+    print("tgmath.py 파일을 실행하셨습니다")
